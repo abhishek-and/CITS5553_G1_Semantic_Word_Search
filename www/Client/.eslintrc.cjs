3929715bf30312cbd174ea0998c12d82581fc92c
Nicholas Choong <nicholascwc1997@gmail.com>
2023-09-12 21:54:36 +0800

    added more config

--- www/Client/.eslintrc.cjs
+++ www/Client/.eslintrc.cjs
@@ -4,7 +4,12 @@ const config = {
   parserOptions: {
     project: true,
   },
-  plugins: ["@typescript-eslint"],
+  plugins: [
+    "@typescript-eslint",
+    "@typescript-eslint",
+    "simple-import-sort",
+    "unused-imports",
+  ],
   extends: [
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended-type-checked",
@@ -23,7 +28,80 @@ const config = {
         fixStyle: "inline-type-imports",
       },
     ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-misused-promises": [
+      2,
+      {
+        checksVoidReturn: {
+          attributes: false,
+        },
+      },
+    ],
+    "no-unused-vars": "off",
+    "no-console": ["warn", { allow: ["error"] }],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+
+    "react/jsx-curly-brace-presence": [
+      "warn",
+      { props: "never", children: "never" },
+    ],
+
+    //#region  //*=========== Unused Import ===========
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "warn",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+    //#endregion  //*======== Unused Import ===========
+
+    //#region  //*=========== Import Sort ===========
+    "simple-import-sort/exports": "warn",
+    "simple-import-sort/imports": [
+      "warn",
+      {
+        groups: [
+          // ext library & side effect imports
+          ["^react$", "^next", "^@?\\w", "^\\u0000"],
+          // Lib and hooks
+          ["^@lib", "^@hooks"],
+          // static data
+          ["^@data"],
+
+          // pages
+          ["^pages"],
+          // components
+          ["^@components", "^@container"],
+          // zustand store
+          ["^@store"],
+          // Other imports
+          ["^@"],
+          // relative paths up until 3 level
+          [
+            "^\\./?$",
+            "^\\.(?!/?$)",
+            "^\\.\\./?$",
+            "^\\.\\.(?!/?$)",
+            "^\\.\\./\\.\\./?$",
+            "^\\.\\./\\.\\.(?!/?$)",
+            "^\\.\\./\\.\\./\\.\\./?$",
+            "^\\.\\./\\.\\./\\.\\.(?!/?$)",
+          ],
+          ["^@types"],
+          // public
+          ["^public"],
+          // other that didnt fit in
+          ["^"],
+          // {s}css files
+          ["^.+\\.s?css$"],
+        ],
+      },
+    ],
+    //#endregion  //*======== Import Sort ===========
   },
 };
 
