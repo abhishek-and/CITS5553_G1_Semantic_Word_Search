e9caa4da7f00da246bf928eb025b83120056b7ec
Nicholas Choong <nicholascwc1997@gmail.com>
2023-10-14 05:50:55 +0800

    match doc names

--- www/Client/src/components/cardlist.tsx
+++ www/Client/src/components/cardlist.tsx
@@ -7,28 +7,29 @@ import {
   CardTitle,
 } from "~/components/ui/card";
 
-const CardList = (doc: any) => {
+const CardList = ({ doc }: any) => {
+  console.log(doc);
   return (
-    <Card
-      key={doc.id}
-      className="w-full transform rounded-lg border border-gray-300 bg-white shadow-md transition duration-500 ease-in-out hover:scale-105 hover:shadow-lg"
-    >
+    <Card className="w-full transform rounded-lg border border-gray-300 bg-white shadow-md transition duration-500 ease-in-out hover:scale-105 hover:shadow-lg">
       <CardHeader>
-        <CardTitle>{doc.documentName}</CardTitle>
+        <CardTitle>{doc.contract_title}</CardTitle>
         {/* <CardDescription>Card Description</CardDescription> */}
       </CardHeader>
       <CardContent>
-        <p className="text-sm text-gray-600">Document ID #: {doc.documentID}</p>
         <p className="text-sm text-gray-600">
-          Similarity Score: {doc.similarityScore}
+          Document ID #: {doc.reference_number}
         </p>
+        <p className="text-sm text-gray-600">
+          Similarity Score: {doc.similarity_score}
+        </p>
+        <p className="text-sm text-gray-600">Supplier: {doc.supplier_name}</p>
         {/* <p className="text-sm text-gray-600">
 UNSPSC Code: {doc["UNSPSC Code"]}
 </p> */}
       </CardContent>
-      {/* <CardFooter>
-<p>Card Footer</p>
-</CardFooter> */}
+      <CardFooter>
+        <p>{doc.sentence_piece}</p>
+      </CardFooter>
     </Card>
   );
 };
