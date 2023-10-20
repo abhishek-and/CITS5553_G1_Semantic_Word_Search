51caa695c15db987ca30af6fcb94c294ecbcd4fc
Nicholas Choong <nicholascwc1997@gmail.com>
2023-10-21 06:09:42 +0800

    better display of relevant info

--- www/Client/src/components/chart/line.tsx
+++ www/Client/src/components/chart/line.tsx
@@ -33,20 +33,28 @@ type CustomTooltip = LineProps["customTooltip"];
 const customTooltip: CustomTooltip = ({ payload, active }) => {
   if (!active || !payload) return null;
   return (
-    <div className="mt-64 w-80 rounded-tremor-default border border-tremor-border bg-tremor-background p-2 text-tremor-default shadow-tremor-dropdown">
+    <div className="mt-80 w-80 rounded-tremor-default border border-tremor-border bg-tremor-background p-2 text-tremor-default shadow-tremor-dropdown">
       {payload.length >= 1 && (
         <div className="flex flex-1 space-x-2.5">
           <div
             className={`flex w-auto flex-col bg-${payload[0]?.color}-500 rounded`}
           />
           <div className="space-y-1">
-            <p className="text-tremor-content">{payload[0]?.dataKey}</p>
+            <p className="text-tremor-content">Similarity Score</p>
             <p className="font-medium text-tremor-content-emphasis">
-              {payload[0]?.value}
+              {Math.round(Number(payload[0]?.value) * 10000) / 10000}
             </p>
-            <p className="text-tremor-content">Tender Closing Date</p>
+            <p className="text-tremor-content">Total Contracts</p>
             <p className="font-medium text-tremor-content-emphasis">
-              {payload[0]?.payload.tender_closing_date}
+              {Number(payload[0]?.payload.row_id) + 1}
+            </p>
+            <p className="text-tremor-content">Reference Number</p>
+            <p className="font-medium text-tremor-content-emphasis">
+              {payload[0]?.payload.reference_number}
+            </p>
+            <p className="text-tremor-content">Contract Title</p>
+            <p className="font-medium text-tremor-content-emphasis">
+              {payload[0]?.payload.contract_title}
             </p>
             <p className="text-tremor-content">Sentence Piece</p>
             <p className="font-medium text-tremor-content-emphasis">
